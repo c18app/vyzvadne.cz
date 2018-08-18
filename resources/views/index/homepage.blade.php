9e1b17ed235dc6ad7a9b36172ff65a7a97bac4cd
Michal Nosavcov <mnosavcov@gmail.com>
2018-08-18 19:28:45 +0200

    update text

--- resources/views/index/homepage.blade.php
+++ resources/views/index/homepage.blade.php
@@ -9,10 +9,15 @@
         vaše dnešní výzva:
     </div>
     <h3>{{ $vyzva }}</h3>
-    <div>
+    <div style="margin-top: 25px;">
         výhody registrace:
+        <ul>
+            <li>Můžete odkládat výzvy, které se vám zdají příliš náročné na dobu až budete připraven/a.</li>
+            <li>Můžete si odškrtnout výzvu jako splněnou.</li>
+            <li>Vidíte historii svých výzev.</li>
+        </ul>
     </div>
-    <div>
+    <div style="margin-top: 25px;">
         @if ($errors->any())
             @else
         <button type="button" class="btn btn-success" onclick="$(this).slideUp(); $('form').slideDown();">Chci přidat novou výzvu</button>
